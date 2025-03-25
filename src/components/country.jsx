88d18aafba7eed5d47a1dbe9ebe36b6ed133b06a
Rabiul-devlop-hub <rabiulawalcoder@gmail.com>
2025-03-25 23:00:59 +0600

    add a button for visited or not

--- src/components/country.jsx
+++ src/components/country.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react"
+
 export default function Country({pro}){
     const {name,flags,languages,population}=pro
 console.log(pro)
+const [visited,setVisited]= useState(false)
+const visitHandler = ()=>{
+    setVisited(true)
+}
 return(
 <div className="box">
     <h1>Name: {name.common}</h1>
     <h1>languages:{languages?.eng}</h1>
     <h1 className="highLight">population:{population}</h1>
    <img src={flags.png} alt="" />
+   <button onClick={visitHandler}>visited</button>
+   {visited ? 'yes i visit' : 'i want to visit'}
 </div>
 )
 }
